6c71444410a177e6f5a53f61931a4c5dbdbfc654
Maxim Murygin <muriginm@gmail.com>
2015-07-25 21:14:35 +0600

    add drawing with different colors

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -4,8 +4,11 @@ var gl,
     canvas,
     vIndex = 0,
     vBuffer,
+    cBuffer,
     drawingMode, 
-    curvesEnds = [];
+    curvesEnds = [],
+    currentColor,
+    colorPicker;
 
 $(document).ready(function (){
     init();
@@ -32,12 +35,27 @@ function init () {
     gl.vertexAttribPointer(program.a_position, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(program.a_position);
 
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 16 * 10000, gl.STATIC_DRAW);
+
+    program.a_color = gl.getAttribLocation(program, 'a_color');
+    gl.vertexAttribPointer(program.a_color, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(program.a_color);
+
     $(canvas).on('mousedown', onCanvasMouseDown)
              .on('mousemove', onCanvasMouseMove);
     $(document).on('mouseup', onDocumentMouseUp);
-    
+
+    colorPicker = $('#color-picker').change(onColorChange);
+    currentColor = getGlColor(colorPicker.val());
+
     render();
 };
+
+function onColorChange () {
+    currentColor = getGlColor(colorPicker.val());
+}
 
 function onCanvasMouseDown () {
     drawingMode = true;
@@ -56,18 +74,33 @@ function addCurrentPotinToBuffer (event) {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec2'] * vIndex, flatten(clickPosition));
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * vIndex, flatten(currentColor));
+
     vIndex++;
     render();
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function getGlColor (hexColor) {
+    var rgbColor = hexToRgb(hexColor);
+
+    return vec4(rgbColor.r / 255.0, rgbColor.g / 255.0, rgbColor.b / 255.0, 1.0);
 }
 
 function onDocumentMouseUp () {
     drawingMode = false;
 
     curvesEnds.push(vIndex);
-}
-
-function onCanvasClick (event) {
-
 }
 
 /**
@@ -101,8 +134,6 @@ function getGlVertexPosition (event, parentCanvas) {
 
 function render()
 {
-    console.log(curvesEnds);
-
     gl.clear( gl.COLOR_BUFFER_BIT ); 
 
     if(vIndex <= 1) return;
